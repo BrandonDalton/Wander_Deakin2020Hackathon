980b69bee89b2f57faa10e136b83e5d813cf8ebe
Brandon D <BrandonDalton@users.noreply.github.com>
2020-04-18 10:44:21 +1000

    Added Timer Functionality and Styling

--- wander-app/src/App.js
+++ wander-app/src/App.js
@@ -4,6 +4,18 @@ import Countdown from 'react-countdown-now';
 import  Timer from './Timer';
 import Goal from './Goal';
 function App() {
+
+  let API_Key = "AIzaSyBzhUgLr6qbOXkkhJZ8WFwtGI6Il0GS_J0";
+
+  // const script = document.createElement("script");
+
+  //   script.src = `https://maps.googleapis.com/maps/api/js?key=${API_Key}&libraries=places`;
+  //   script.async = true;
+
+  //   document.body.appendChild(script);
+  //   console.log(fetch())
+    
+
   return (
    <div className="homePage--container">
           Home Page
--- wander-app/src/Timer.js
+++ wander-app/src/Timer.js
@@ -3,21 +3,69 @@ import './App.scss';
 import Countdown from 'react-countdown-now';
 
 class Timer extends React.Component {
+    clockRef = null;
+
+    constructor(props) {
+        super(props);
+        this.state = {value: '', onStart: true};
+        
+        this.setClockRef = this.setClockRef.bind(this);
+        this.start = this.start.bind(this);
+        this.pause = this.pause.bind(this);
+        this.clear = this.clear.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+      start() {
+        this.clockRef.start();
+    }
+
+    pause() {
+        this.clockRef.pause();
+    }
+
+    clear() {
+        console.log(this.state)
+            this.setState({value: ''});
+    }
+
+    setClockRef(ref) {
+        this.clockRef = ref;
+    }
+
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
     
+      handleSubmit(event) {
+        console.log('Number Submitted ' + this.state.value);
+        event.preventDefault();
+      }
+
 
     render() {
       return (
         <div className="homePage--timerSection">
-        First Section
         <div className="timerSection--timerContainer">
-          <div>
-           <h1>Timer</h1>
-           <Countdown date={Date.now() + 10000} />,
+          <div className="timerSection--timer">
+           <h1>Get Productive</h1>
+           <Countdown ref={this.setClockRef} autoStart={false} precision={0} date={Date.now() + (this.state.value * 1000)} />
           </div>
         </div>
         <div className="timerSection--timerControls">
-          <div>
-            <button>Start</button>
+          <div className="timerControls--formContainer">
+          <form onSubmit={this.handleSubmit}>
+        <label>
+          Seconds:
+          <input type="number" value={this.state.value} onChange={this.handleChange} />
+        </label>
+      </form>
+          </div>
+          <div className="timerControls--buttons">
+              <div className="button" onClick={this.start}>Start Clock</div>
+              <div className="button" onClick={this.pause}>Pause Clock</div>
+              <div className="button" onClick={this.clear}>Clear Clock</div>
           </div>
         </div>
       </div>
